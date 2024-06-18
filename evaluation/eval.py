b409e1133b9db6edaea422f33d66790a750f3259
kushal-10 <kushalkoshti11@gmail.com>
2024-06-18 20:53:56 +0200

    add ModelRuns backend

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -1,39 +1,26 @@
 """
 TODOS - 1806
 1) Use the adapters to do inference on an image
-- Try AutoAdapterModel loading for llava1.5
-2) Set up evaluation pipeline
+- Try AutoAdapterModel loading for llava1.5 [DONE]
+
+2) Set up evaluation pipeline [ON THIS]
+We have a backend up and running, Need to write scoring function/ Metrics
+Single flow to generate responses, without multiple times model loading
+
 3) Use the adapters, base model and generate two results
 """
 
-from transformers import AutoModelForVision2Seq, AutoProcessor
-from peft import PeftModel
-from PIL import Image
-import torch
+from evaluation.runs import ModelRuns
 
-model_name = "llava-hf/llava-1.5-13b-hf"
-base_model = AutoModelForVision2Seq.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
+model_id = "llava-hf/llava-1.5-13b-hf"
+adapter_id = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Load fine-tuned adapter
-# adapter_path = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"  # Replace with your adapter path
-# model = PeftModel.from_pretrained(base_model, adapter_path, device_map="auto", torch_dtype=torch.float16)
-model = base_model
+model = ModelRuns(model_id, adapter_id)
 
 prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
 image_file = "data/easy/test/board_0/images/step_0.png"
 
-# Load Processor from HF directly
-processor = AutoProcessor.from_pretrained(model_name, device_map="auto")
-
-raw_image = Image.open(image_file).convert('RGB')
-inputs = processor(prompt, raw_image, return_tensors='pt').to(device)
-
-output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
-print(processor.decode(output[0][2:], skip_special_tokens=True))
-
-
-
+response = model.generate_response(prompt, image_file)
+print(f"Response: {response}")
 
 
---
+++ evaluation/runs.py
@@ -0,0 +1,43 @@
+## Backend for running the models
+from transformers import AutoModelForVision2Seq, AutoProcessor
+from peft import PeftModel
+from PIL import Image
+import torch
+
+class ModelRuns():
+    def __init__(self, model_id: str, adapter_path: str = ""):
+        """
+        Args:
+            model_id :  Huggingface ID for the model
+            adapter_path: If using the fine tuned version, specify the path to the adapter directory
+            image_path : Path to the image
+            prompt : Prompt value (string)
+        """
+
+        self.model_id = model_id
+        self.adapter_path = adapter_path
+
+        # Load Model and Processor
+        self.model = AutoModelForVision2Seq.from_pretrained(self.model_id, device_map="auto", torch_dtype=torch.float16)
+        if self.adapter_path:
+            self.model = PeftModel.from_pretrained(self.model, self.adapter_path, device_map="auto", torch_dtype=torch.float16)
+
+        self.processor = AutoProcessor.from_pretrained(self.model_id, device_map="auto")
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def generate_response(self, image_path: str, prompt: str):
+        """
+        Args:
+            image_path: Path to the image
+            prompt: Prompt value (string)
+        Returns:
+            response: Model Response (After Processing it to a single word)
+        """
+
+        image = Image.open(image_path).convert("RGB")
+        inputs = self.processor(image, return_tensors="pt").to(self.device)
+        outputs = self.model.generate(**inputs, max_new_tokens=10, do_sample=False)
+        response = processor.decode(output[0][2:], skip_special_tokens=True)
+
+        return response
