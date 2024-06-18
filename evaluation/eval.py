1b12b2a1d654e2ef110e88a8a3fb3823c0d604cc
kushal-10 <kushalkoshti11@gmail.com>
2024-06-18 20:32:34 +0200

    adapter inference works, try base model inference

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -17,8 +17,9 @@ base_model = AutoModelForVision2Seq.from_pretrained(model_name, device_map="auto
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load fine-tuned adapter
-adapter_path = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"  # Replace with your adapter path
-model = PeftModel.from_pretrained(base_model, adapter_path, device_map="auto", torch_dtype=torch.float16)
+# adapter_path = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"  # Replace with your adapter path
+# model = PeftModel.from_pretrained(base_model, adapter_path, device_map="auto", torch_dtype=torch.float16)
+model = base_model
 
 prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
 image_file = "data/easy/test/board_0/images/step_0.png"
