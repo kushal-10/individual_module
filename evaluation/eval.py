45f82553aa2f32780e8329d98ab86a565a3efcaf
kushal-10 <kushalkoshti11@gmail.com>
2024-06-18 20:29:11 +0200

    manage weights distribution over GPU

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -12,26 +12,22 @@ from PIL import Image
 import torch
 
 model_name = "llava-hf/llava-1.5-13b-hf"
-base_model = AutoModelForVision2Seq.from_pretrained(model_name)
+base_model = AutoModelForVision2Seq.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load fine-tuned adapter
 adapter_path = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"  # Replace with your adapter path
-model = PeftModel.from_pretrained(base_model, adapter_path).to(0)
+model = PeftModel.from_pretrained(base_model, adapter_path, device_map="auto", torch_dtype=torch.float16)
 
 prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
 image_file = "data/easy/test/board_0/images/step_0.png"
 
-# model = LlavaForConditionalGeneration.from_pretrained(
-#     model_id,
-#     torch_dtype=torch.float16,
-#     low_cpu_mem_usage=True,
-# ).to(0)
-
 # Load Processor from HF directly
-processor = AutoProcessor.from_pretrained(model_name)
+processor = AutoProcessor.from_pretrained(model_name, device_map="auto").to(device)
 
 raw_image = Image.open(image_file).convert('RGB')
-inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
+inputs = processor(prompt, raw_image, return_tensors='pt')
 
 output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
 print(processor.decode(output[0][2:], skip_special_tokens=True))
