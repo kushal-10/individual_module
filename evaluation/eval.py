0d6bd70908d26628c4f77348ea1591afb0b7ee60
kushal-10 <kushalkoshti11@gmail.com>
2024-06-18 20:55:39 +0200

    test multiple runs

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -19,8 +19,18 @@ model = ModelRuns(model_id, adapter_id)
 
 prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
 image_file = "data/easy/test/board_0/images/step_0.png"
-
-response = model.generate_response(prompt, image_file)
+response = model.generate_response(image_file, prompt)
 print(f"Response: {response}")
+
+prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
+image_file = "data/easy/test/board_0/images/step_1.png"
+response = model.generate_response(image_file, prompt)
+print(f"Response: {response}")
+
+prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
+image_file = "data/easy/test/board_0/images/step_2.png"
+response = model.generate_response(image_file, prompt)
+print(f"Response: {response}")
+
 
 
