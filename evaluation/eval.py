67d6effa72d2de80b6011b640b5fe8e745e33ae3
kushal-10 <kushalkoshti11@gmail.com>
2024-06-18 20:21:46 +0200

    model to cuda 0

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -8,24 +8,15 @@ TODOS - 1806
 
 from transformers import AutoModelForVision2Seq, AutoProcessor
 from peft import PeftModel
+from PIL import Image
+import torch
 
-# Load base model
-# model_name = "../llava-1.5-13b-hf"
 model_name = "llava-hf/llava-1.5-13b-hf"
 base_model = AutoModelForVision2Seq.from_pretrained(model_name)
 
 # Load fine-tuned adapter
 adapter_path = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"  # Replace with your adapter path
-model = PeftModel.from_pretrained(base_model, adapter_path)
-
-
-# import requests
-from PIL import Image
-#
-import torch
-# from transformers import AutoProcessor, LlavaForConditionalGeneration
-
-model_id = "llava-hf/llava-1.5-13b-hf"
+model = PeftModel.from_pretrained(base_model, adapter_path).to(0)
 
 prompt = "<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the red P shaped piece located on top left of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\n ASSISTANT:"
 image_file = "data/easy/test/board_0/images/step_0.png"
@@ -36,7 +27,8 @@ image_file = "data/easy/test/board_0/images/step_0.png"
 #     low_cpu_mem_usage=True,
 # ).to(0)
 
-processor = AutoProcessor.from_pretrained(model_id)
+# Load Processor from HF directly
+processor = AutoProcessor.from_pretrained(model_name)
 
 raw_image = Image.open(image_file).convert('RGB')
 inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
