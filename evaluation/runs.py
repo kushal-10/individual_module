e76c1dfadb14fc3a5d9a8dd1ee47a2935585ac61
kushal-10 <kushalkoshti11@gmail.com>
2024-06-19 13:49:38 +0200

    eval base model on gpt prompt

--- evaluation/runs.py
+++ evaluation/runs.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     adapter_id = "../LLaVA/checkpoints/llava-v1.5-13b-task-lora"
 
     # Initialize Model
-    # base_model = ModelRuns(model_id)
-    adapter_model = ModelRuns(model_id, adapter_id)
+    base_model = ModelRuns(model_id)
+    # adapter_model = ModelRuns(model_id, adapter_id)
 
-    eval_model = adapter_model
+    eval_model = base_model
 
     # Initialize locations
     root = 'data'
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     boards.sort()
 
     # Initialize data to collect
-    save_name = 'llava15_base_test.csv'
+    save_name = 'base_prompt2.csv'
     actions = []
     gts = []
     board_numbers = []
@@ -56,7 +56,9 @@ if __name__ == "__main__":
         if len(re_split) == 7:
             position += ' ' + re_split[6]
         # prompt = f"<image>\nYou are an intelligent agent playing a pentomino game. You are given a board with 20 x 20 grids and a target piece. Your spawn location is represented by the black circle on the board. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the {colour} {shape} shaped piece located on {position} of the board. Only respond in one word what next step will you take from ['left', 'right', 'up', 'down', 'grip']\nASSISTANT:"
-        prompt = f"USER: <image>\nYou are given a board with 20 x 20 grids and a target piece. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your spawn location is represented by the black circle on the board and you can only move ['left', 'right', 'up', 'down'] - Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the {colour} {shape} shaped piece located on {position} of the board. Only respond in one word.\nASSISTANT:"
+        # prompt = f"USER: <image>\nYou are given a board with 20 x 20 grids and a target piece. There are 3 more distractor pieces. These pieces resemble one of the letters from ['P', 'T', 'U', 'W', 'X', 'Z']. Your spawn location is represented by the black circle on the board and you can only move ['left', 'right', 'up', 'down'] - Your task is to take a step or grip the piece. The step should be towards the direction of the target piece. Proceed to take the {colour} {shape} shaped piece located on {position} of the board. Only respond in one word.\nASSISTANT:"
+        gpt_prompt = f"USER: <image>\nNavigate a 20x20 grid board to grip the {colour} {shape} piece at {position}. Avoid distractor pieces resembling ['P', 'T', 'U', 'W', 'X', 'Z']. Start at the black circle; move ['grip', 'left', 'right', 'up', 'down']. Respond with your next move in only one word.\nASSISTANT:"
+        prompt = gpt_prompt
 
         for image_path in image_paths:
             # At single instance level
